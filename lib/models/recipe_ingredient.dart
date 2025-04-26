2d304f88cd32ea5ac6a286fc4a49555cea392c45
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2025-04-26 11:40:01 -0300

    🗑️remove: Remoção do atributo de Unit da Classe RecipeIngredient

--- lib/models/recipe_ingredient.dart
+++ lib/models/recipe_ingredient.dart
@@ -1,11 +1,6 @@
 class RecipeIngredient {
   final String name;
   final double quantity;
-  final String unit;
 
-  RecipeIngredient({
-    required this.name,
-    required this.quantity,
-    required this.unit,
-  });
+  RecipeIngredient({required this.name, required this.quantity});
 }
