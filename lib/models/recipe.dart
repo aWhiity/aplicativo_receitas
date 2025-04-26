1424fb67db31e3615e6dd44857f324b7e1249c14
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2025-04-26 11:52:18 -0300

    ✨ feat: Adição e alteração de atributos da classe Recipe
    - Adicionada a lista de ingredientes como atributo;
    - Alterado o atributo de descrição para suportar valores nulos.

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -1,13 +1,17 @@
+import 'package:aplicativo_receitas/models/recipe_ingredient.dart';
+
 class Recipe {
-  final String id;
-  final String name;
-  String desc;
+  final String id; //rever o tipo do id
+  String name;
+  List<RecipeIngredient> ingredients;
+  String? desc;
   Duration preparationTime;
   String imagePath;
 
   Recipe({
     required this.id,
     required this.name,
+    required this.ingredients,
     required this.desc,
     required this.preparationTime,
     required this.imagePath,
