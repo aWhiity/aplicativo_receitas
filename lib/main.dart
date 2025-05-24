954020c951147e7017cdc2abb6aca9805010b6d6
barbarahw <127110674+barbarahw@users.noreply.github.com>
2025-05-24 15:21:17 -0300

    🔧 chore: configurações para uso do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  );
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     MultiProvider(
