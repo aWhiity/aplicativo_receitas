e4b86892e1b86420dbad25def9b0318808a55b41
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2025-05-24 20:12:39 -0300

    ✨ feat: Adição de métodos para melhor controle dos objetos
    - Métodos ToMap e FromMap adicionados

--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -18,6 +18,37 @@ class Recipe {
     this.preparationTime,
     required this.instructions,
     required this.imagePath,
-    this. isFav = false
+    this.isFav = false,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'ingredients': ingredients.map((i) => i.toMap()).toList(),
+      'desc': desc,
+      'preparationTime': preparationTime?.inMinutes,
+      'instructions': instructions,
+      'imagePath': imagePath,
+      'isFav': isFav,
+    };
+  }
+
+  factory Recipe.fromMap(String id, Map<String, dynamic> map) {
+    return Recipe(
+      id: id,
+      name: map['name'],
+      ingredients:
+          (map['ingredients'] as List<dynamic>)
+              .map((e) => RecipeIngredient.fromMap(e))
+              .toList(),
+      desc: map['desc'],
+      preparationTime:
+          map['preparationTime'] != null
+              ? Duration(minutes: map['preparationTime'])
+              : null,
+      instructions: map['instructions'],
+      imagePath: map['imagePath'],
+      isFav: map['isFav'] ?? false,
+    );
+  }
 }
--- lib/models/recipe_ingredient.dart
+++ lib/models/recipe_ingredient.dart
@@ -3,4 +3,12 @@ class RecipeIngredient {
   String quantity;
 
   RecipeIngredient({required this.name, required this.quantity});
+
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'quantity': quantity};
+  }
+
+  factory RecipeIngredient.fromMap(Map<String, dynamic> map) {
+    return RecipeIngredient(name: map['name'], quantity: map['quantity']);
+  }
 }
