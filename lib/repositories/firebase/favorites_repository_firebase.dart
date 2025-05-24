6c48bf5d80e277aafc9776e88f037de56f195c65
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2025-05-24 20:19:48 -0300

    ✨ feat: Adição das classes de Repositórios  no Firebase

---
+++ lib/repositories/firebase/favorites_repository_firebase.dart
@@ -0,0 +1,5 @@
+import 'package:aplicativo_receitas/repositories/recipes_repository.dart';
+import 'package:flutter/foundation.dart';
+
+class FavoritesRepositoryFirebase extends ChangeNotifier
+    implements RecipesRepository {}
