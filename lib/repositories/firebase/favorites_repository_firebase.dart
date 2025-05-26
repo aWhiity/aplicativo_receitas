98fe804ddf0f859920d1e19ba594d666348c554f
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2025-05-26 20:28:14 -0300

    ✨ feat: Adição da classe de controle dos Favoritos no Firebase

--- lib/repositories/firebase/favorites_repository_firebase.dart
+++ lib/repositories/firebase/favorites_repository_firebase.dart
@@ -1,5 +1,83 @@
-import 'package:aplicativo_receitas/repositories/recipes_repository.dart';
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:aplicativo_receitas/models/recipe.dart';
+import 'package:aplicativo_receitas/repositories/favorites_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
 class FavoritesRepositoryFirebase extends ChangeNotifier
-    implements RecipesRepository {}
+    implements FavoritesRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final List<Recipe> _favorites = [];
+
+  @override
+  List<Recipe> get recipes => UnmodifiableListView(_favorites);
+
+  late final String _userId;
+  late final CollectionReference _favoritesCollection;
+  StreamSubscription? _subscription;
+
+  FavoritesRepositoryFirebase() {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw Exception('Usuário não autenticado');
+    }
+    _userId = user.uid;
+    _favoritesCollection = _firestore
+        .collection('users')
+        .doc(_userId)
+        .collection('recipes');
+
+    _subscription = _favoritesCollection
+        .where('isFav', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+          _favorites.clear();
+          for (var doc in snapshot.docs) {
+            _favorites.add(
+              Recipe.fromMap(doc.id, doc.data() as Map<String, dynamic>),
+            );
+          }
+          notifyListeners();
+        });
+  }
+
+  /* @override
+  Stream<List<Recipe>> getFavoriteRecipes() {
+    return _favoritesCollection
+        .where('isFav', isEqualTo: true)
+        .snapshots()
+        .map(
+          (snapshot) =>
+              snapshot.docs
+                  .map(
+                    (doc) => Recipe.fromMap(
+                      doc.id,
+                      doc.data()! as Map<String, dynamic>,
+                    ),
+                  )
+                  .toList(),
+        );
+  }*/
+
+  @override
+  Future<void> toggleFavorite(Recipe recipe, bool isFavorite) async {
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+    final docRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('recipes')
+        .doc(recipe.id);
+
+    await docRef.update({'isFav': isFavorite});
+    
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+}
